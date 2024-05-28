73ed75982c0aa758bcd41c3eb2c1672f5eaad477
ElsaMayaBahri <elsabahri27@gmail.com>
2024-05-28 14:47:23 +0700

    deskripsi_wisata

---
+++ deskripsi_wisata.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deskripsi Wisata</title>
+    <link rel="stylesheet" href="css/user2.css">
+</head>
+<body>
+
+<section class="deskripsi-wisata">
+
+    <h1 class="heading-title">Wisata Top</h1>
+
+    <div class="box-container">
+        <!-- Paket 1 -->
+        <div class="box">
+            <div class="image">
+                <img src="asset/1 hari.jpg" alt="Gambar Wisata 1">
+            </div>
+            <div class="content">
+                <h3>Adventure Juoss</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <a href="user_page.php" class="btn">Book Now</a>
+            </div>
+        </div>
+
+        <!-- Paket 2 -->
+        <div class="box">
+            <div class="image">
+                <img src="asset/2 hari.jpg" alt="Gambar Wisata 2">
+            </div>
+            <div class="content">
+                <h3>Relaxing Getaway</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <a href="user_page.php" class="btn">Book Now</a>
+            </div>
+        </div>
+
+        <!-- Paket 3 -->
+        <div class="box">
+            <div class="image">
+                <img src="asset/3 hari.jpg" alt="Gambar Wisata 3">
+            </div>
+            <div class="content">
+                <h3>City Tour</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <a href="user_page.php" class="btn">Book Now</a>
+            </div>
+        </div>
+
+        <!-- Tambahkan lebih banyak paket sesuai kebutuhan -->
+    </div>
+ <!-- Paket 4 -->
+ <div class="box">
+            <div class="image">
+                <img src="asset/Banyu Urip Mangrove Center.jpg" alt="Gambar Wisata 3">
+            </div>
+            <div class="content">
+                <h3>City Tour</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <a href="user_page.php" class="btn">Book Now</a>
+            </div>
+        </div>
+<!-- Paket 3 -->
+<div class="box">
+            <div class="image">
+                <img src="asset/Bajak Laut.jpg" alt="Gambar Wisata 3">
+            </div>
+            <div class="content">
+                <h3>City Tour</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <a href="user_page.php" class="btn">Book Now</a>
+            </div>
+        </div>
+</section>
+
+</body>
+</html>
