d6b2a9bf29b5be19f04ce70f14b46700a69e0e3c
Rkamaisya <rikhamaisya@gmail.com>
2024-06-11 20:12:50 +1400

    user

--- user_page.php
+++ user_page.php
@@ -47,8 +47,6 @@ if(!isset($_SESSION['user_name']))
       crossorigin="anonymous"
     ></script>
 
-   
-    
 
     </head>
     <body>
@@ -66,22 +64,15 @@ if(!isset($_SESSION['user_name']))
             </a>
             <ul>   <!--NAV BAR-->
                 <li><a href="#home">Home</a></li>
-                <li><a href="#aboutus">About Us</a></li>
+                <li><a href="#aboutus">Galarry</a></li>
                 <li>
                 <a href="#">Destinasi <i class="ti-angle-down"></i></a>
                 <ul class="submenu">
-                    <li><a href="Pantai">Pantai</a></li>
+                    <li><a href="Kategori_Pantai.html">Pantai</a></li>
                     <li><a href="Sejarah">Sejarah</a></li>
               </ul>
               </li>
-                      <!-- NAV BAR-->
               <li>
-                <a href="#">Aktivitas <i class="ti-angle-down"></i></a>
-                <ul class="submenu">
-                    <li><a href="Oleh-oleh">Oleh-oleh</a></li>
-                    <li><a href="Event">Event</a></li>
-                    <li><a href="Tips Liburan">Tips Liburan</a></li>
-              </ul>
               </li>     <!-- NAV BAR -->
                 <li><a href="#blog">Blog</a></li>
                 <li><a href="#Rekomendasi">Rekomendasi</a></li>
@@ -111,17 +102,10 @@ if(!isset($_SESSION['user_name']))
       </header>
       <main>
         <section id="aboutus">
-          <div class="layar-dalam">
-            <h3>About Us</h3>
-            <p class="ringkasan">
-                Kami dari kelompok 10 mengerjakan tugas akhir dengan studi kasus Web wisata
-            </p>
-            <div class="konten-isi">
-              <p>
-                Demi menyelesaikan tugas mata kuliah pemogramman Web, kita kelompok 10 mengerjakan tugas akhir yaitu
-                membuat web dengan studi kasus web wisata
-              </p>
-            </div>
+        <div class="main-txt">
+          <h1><span>Gallary</span>Wisata</h1>
+        </div>
+            
           </div>
         </section>
          <!-- Galerry -->
@@ -135,7 +119,54 @@ if(!isset($_SESSION['user_name']))
           <div><img src="asset/Pantai Delegan.png" /></div>
           <div><img src="asset/Wagos Wisata Alam Gosari.png" /></div>
         </section>
-        
+ 
+          <!-- Section Gallary Start -->
+    <section class="gallary" id="gallary">
+      <div class="container">
+        <div class="row" style="margin-top: 30px;">
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card">
+              <img src="asset/Dynasty Water World.jpg" alt="" height="230px">
+            </div>
+          </div>
+          
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card">
+              <img src="asset/Makam Sunan Syeikh Maulana Malik Ibrahim.png" alt="" height="230px">
+            </div>
+          </div>
+          
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card">
+              <img src="asset/Pulau Mengare.jpg" alt="" height="230px">
+            </div>
+          </div>
+        </div>
+
+
+        <div class="row" style="margin-top: 30px;">
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card">
+              <img src="asset/Bukit Kapur Gosari.jpg" alt="" height="230px">
+            </div>
+          </div>
+          
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card">
+              <img src="asset/Bajak Laut.jpg" alt="" height="230px">
+            </div>
+          </div>
+          
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card">
+              <img src="asset/Wagos Wisata Alam Gosari.jpg" alt="" height="230px">
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- Section Gallary End -->
         <section class="quote">
           <div class="layar-dalam">
             <p>Tak hanya sebatas tanah dan bangunan, 
@@ -211,7 +242,262 @@ if(!isset($_SESSION['user_name']))
             </div>
           </div>
 
+<!-- Section Packages Start -->
+<section class="Rekomendasi" id="Rekomendasi">
+      <div class="container">
+        
+        <div class="main-txt">
+          <h1><span>REKO</span>MENDASI</h1>
+        </div>
+        <div class="container">
+        <div class="row" style="margin-top: 30px;">
+            <!-- Card 1 -->
+            <div class="col-md-4 py-3">
+                <div class="card">
+                    <img src="asset/1 hari.jpg" class="card-img-top" alt="1 Hari">
+                    <div class="card-body">
+                        <h3>1 Hari</h3>
+                        <p>Wisata Bukit Kapur Gosari, Klenteng Kim Hin, dan Bajak Laut!</p>
+                        <div class="star">
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                        <h6>Price: <strong>300.000</strong></h6>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal1">
+                            See Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- Card 2 -->
+            <div class="col-md-4 py-3">
+                <div class="card">
+                    <img src="asset/2 hari.jpg" class="card-img-top" alt="2 Hari">
+                    <div class="card-body">
+                        <h3>2 Hari</h3>
+                        <p>Hari Pertama: Bukit Jamur dan Pulau Mangare</p>
+                        <p>Hari Kedua: Wagos wisata alam dan Pantai</p>
+                        <div class="star">
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                        <h6>Price: <strong>500.000</strong></h6>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal2">
+                            See Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- Card 3 -->
+            <div class="col-md-4 py-3">
+                <div class="card">
+                <img src="asset/1 hari.jpg" class="card-img-top" alt="3 Hari">
+                    <div class="card-body">
+                        <h3>3 Hari</h3>
+                        <p>Hari Pertama: Banyu Urip dan Telaga Ngipik</p>
+                        <p>Hari Kedua: Bukit Kapur dan Pantai Karang Kering</p>
+                        <p>Hari Ketiga: Makam Sunan Giri alam dan Dynasty Water World</p>
+                        <div class="star">
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star checked"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                        <h6>Price: <strong>700.000</strong></h6>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal2">
+                            See Now
+                        </button>
+                    </div>
+               
 
+        <!-- Modal 1 -->
+        <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="modal1Title" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <img src="asset/Bukit Kapur Gosari.jpg" class="img-thumbnail" alt="1 Hari">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <ul>
+                            <li><strong>Bukit Kapur Gosari</strong>
+                                <ul>
+                                   Lokasi: Gosari, Gresik, Jawa Timur</li>
+                                   Tiket Masuk: Sekitar Rp 5.000 - Rp 10.000 per orang</li>
+                                   Souvenir: Tersedia berbagai macam souvenir lokal di sekitar area wisata</li>
+                                </ul>
+                            </li>
+                            <li><strong>Klenteng Kim Hin</strong>
+                                <ul>
+                                    Lokasi: Jl. A. Yani No. 93, Gresik, Jawa Timur</li>
+                                    Tiket Masuk: Biasanya gratis, namun sumbangan sukarela dapat diberikan</li>
+                                    Souvenir: Mungkin tersedia di sekitar klenteng</li>
+                                </ul>
+                            </li>
+                            <li><strong>Bajak Laut</strong>
+                                <ul>
+                                   Lokasi: Gresik, Jawa Timur</li>
+                                    Tiket Masuk: Tergantung pada atraksi yang ingin dinikmati, rata-rata sekitar Rp 10.000 - Rp 20.000 per orang</li>
+                                    Souvenir: Mungkin tersedia di sekitar area wisata</li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <p>[Transportasi Kesana:</p>
+                            <li>Anda dapat menggunakan mobil pribadi, taksi, atau menyewa mobil untuk perjalanan dari lokasi Anda ke destinasi wisata. Untuk transportasi lokal di sekitar destinasi, mungkin tersedia ojek atau angkutan umum.</li>
+                        </ul>
+                        <p>Perhatikan Keadaan Cuaca dan Jam Buka:</p>
+                        <ul>
+                            <li>Untuk ketiga wisata buka pukul 08.00-17.00 WIB.</li>
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal 2 -->
+        <div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="modal2Title" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <img src="asset/Wagos Wisata Alam Gosari.jpg" class="img-thumbnail" alt="2 Hari">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Modal content for 2 Hari -->
+                        <!-- Similar to Modal 1 but with relevant content -->
+                    </div>
+                    <div class="modal-footer">
+                    <div class="modal-body">
+                        <ul>
+                        <ul>
+                    <li><strong>Hari 1: Bukit Jamur</strong>
+                        <ul>
+                            <li>Lokasi: Bungah, Gresik, Jawa Timur</li>
+                            <li>Tiket Masuk: Sekitar Rp 15.000 per orang</li>
+                            <li>Souvenir: Tersedia berbagai macam souvenir lokal di sekitar area wisata</li>
+                        </ul>
+                    </li>
+                    <li><strong>Hari 2: Pantai Delegan</strong>
+                        <ul>
+                            <li>Lokasi: Delegan, Panceng, Gresik, Jawa Timur</li>
+                            <li>Tiket Masuk: Sekitar Rp 10.000 per orang</li>
+                            <li>Souvenir: Tersedia berbagai macam souvenir lokal di sekitar area wisata</li>
+                        </ul>
+                    </li>
+                    <li><strong>Hari 3: Wisata Kuliner Gresik</strong>
+                        <ul>
+                            <li>Lokasi: Pusat Kota Gresik, Jawa Timur</li>
+                            <li>Biaya: Sesuai dengan makanan yang dipilih</li>
+                            <li>Souvenir: Tersedia oleh-oleh khas Gresik di sekitar area wisata kuliner</li>
+                        </ul>
+                    </li>
+                </ul>
+                <p><strong>Transportasi Kesana:</strong></p>
+                <ul>
+                    <li>Anda dapat menggunakan mobil pribadi, taksi, atau menyewa mobil untuk perjalanan dari lokasi Anda ke destinasi wisata. Untuk transportasi lokal di sekitar destinasi, mungkin tersedia ojek atau angkutan umum.</li>
+                </ul>
+                <p><strong>Perhatikan Keadaan Cuaca dan Jam Buka:</strong></p>
+                <ul>
+                    <li>Untuk ketiga wisata buka pukul 08.00-17.00 WIB.</li>
+                </ul>
+                    </div>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+ <!-- Modal 3 -->
+ <div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-labelledby="modal3Title" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <img src="asset/1 hari.jpg" class="img-thumbnail" alt="3 Hari">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <ul>
+                            <li><strong>Wisata A</strong>
+                                <ul>
+                                    <li>Lokasi: A, Gresik, Jawa Timur</li>
+                                    <li>Tiket Masuk: Sekitar Rp 10.000 per orang</li>
+                                    <li>Souvenir: Tersedia berbagai macam souvenir lokal di sekitar area wisata</li>
+                                </ul>
+                            </li>
+                            <li><strong>Wisata B</strong>
+                                <ul>
+                                    <li>Lokasi: B, Gresik, Jawa Timur</li>
+                                    <li>Tiket Masuk: Sekitar Rp 15.000 per orang</li>
+                                    <li>Souvenir: Mungkin tersedia di sekitar area wisata</li>
+                                </ul>
+                            </li>
+                            <li><strong>Wisata C</strong>
+                                <ul>
+                                    <li>Lokasi: C, Gresik, Jawa Timur</li>
+                                    <li>Tiket Masuk: Sekitar Rp 20.000 per orang</li>
+                                    <li>Souvenir: Mungkin tersedia di sekitar area wisata</li>
+                                </ul>
+                            </li>
+                            <li><strong>Wisata D</strong>
+                                <ul>
+                                    <li>Lokasi: D, Gresik, Jawa Timur</li>
+                                    <li>Tiket Masuk: Sekitar Rp 25.000 per orang</li>
+                                    <li>Souvenir: Mungkin tersedia di sekitar area wisata</li>
+                                </ul>
+                            </li>
+                            <li><strong>Wisata E</strong>
+                                <ul>
+                                    <li>Lokasi: E, Gresik, Jawa Timur</li>
+                                    <li>Tiket Masuk: Sekitar Rp 30.000 per orang</li>
+                                    <li>Souvenir: Mungkin tersedia di sekitar area wisata</li>
+                                </ul>
+                            </li>
+                            <li><strong>Wisata F</strong>
+                                <ul>
+                                    <li>Lokasi: F, Gresik, Jawa Timur</li>
+                                    <li>Tiket Masuk: Sekitar Rp 35.000 per orang</li>
+                                    <li>Souvenir: Mungkin tersedia di sekitar area wisata</li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <p>Transportasi Kesana:</p>
+                            <li>Anda dapat menggunakan mobil pribadi, taksi, atau menyewa mobil untuk perjalanan dari lokasi Anda ke destinasi wisata. Untuk transportasi lokal di sekitar destinasi, mungkin tersedia ojek atau angkutan umum.</li>
+                        </ul>
+                        <p>Perhatikan Keadaan Cuaca dan Jam Buka:</p>
+                        <ul>
+                            <li>Wisata buka pukul 08.00-17.00 WIB setiap hari.</li>
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+      </div>
+    </section>
+    <!-- Section Packages End -->
 
   <!-- CONCTat -->
         </section>
